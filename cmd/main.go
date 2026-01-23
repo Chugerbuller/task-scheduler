f7924c1b971457f06fe37c7fe65f5a49d65cd5c4
chuga <sprunk59@yandex.ru>
2026-01-23 14:43:52 +0500

    webserver tested

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Ошибка загрузки .env файла, %v", err)
+	}
+	port := os.Getenv("TODO_PORT")
+	if port == "" {
+		port = "7550"
+	}
+
+	router := chi.NewRouter()
+
+	router.Handle("/*", http.FileServer(http.Dir("./web")))
+
+	log.Printf("Server started on port:%s", port)
+
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatalf("Server is shuted down %v", err)
+	}
+}
---
+++ internal/api/api.go
@@ -0,0 +1,7 @@
+package api
+
+import "net/http"
+
+func HandleIndex(w *http.ResponseWriter, r *http.Request) {
+
+}
